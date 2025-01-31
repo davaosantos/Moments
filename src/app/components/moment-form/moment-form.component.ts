0900fdd541060287fab0c34b02d9f9c34841dbbe
david <david_Alisson60@yahoo.com.br>
2025-01-30 22:07:26 -0300

    38 - Criando a tela de edição

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -12,8 +12,10 @@ export class MomentFormComponent implements OnInit {
 
   @Output() onSubmit = new EventEmitter<Moment>();
   @Input() btnText! : string
+  @Input() momentData : Moment | null = null;
 
   momentForm! : FormGroup;
+  
 
   constructor() { }
 
